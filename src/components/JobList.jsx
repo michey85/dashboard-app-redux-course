b96d3df20388fea82e0f95a90c757812ed4e67f6
Mikhail Nepomnyashchiy <nem@kodix.ru>
2022-01-31 11:51:29 +0100

    filters logic to ui

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,12 +1,14 @@
 import {useSelector, useDispatch} from 'react-redux';
 import { JobPosition } from './JobPosition';
-import {selectAllPositions} from 'store/positions/position-selectors';
+import {selectVisiblePositions} from 'store/positions/position-selectors';
+import {selectFitlers} from 'store/filters/filter-selectors';
 
 import {addFilter} from 'store/filters/filter-actions';
 
 const JobList = () => {
   const dispatch = useDispatch();
-  const positions = useSelector(selectAllPositions);
+  const currentFitlers = useSelector(selectFitlers);
+  const positions = useSelector((state) => selectVisiblePositions(state, currentFitlers));
 
   const handleAddFilter = (filter) => {
     dispatch(addFilter(filter));
